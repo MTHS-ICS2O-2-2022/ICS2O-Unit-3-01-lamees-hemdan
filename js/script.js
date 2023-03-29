798339540258885e3498243af0cad2bc84ba962b
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-03-29 17:29:45 +0000

    added favicon

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lamees Hemdan
+// Created on: March 2023
 // This file contains the JS functions for index.html
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// This function calculates the area of a trapezoid
+
+function calculateArea () {
+ // input
+  const base1 = parseInt(document.getElementById('aBase').value)
+  const base2 = parseInt(document.getElementById('bBase').value)
+  const height = parseInt(document.getElementById('height').value)
+
+
+ // process
+  const area = (base1 + base2) / 2 * height
+
+
+ // output
+  document.getElementById('area').innerHTML = 'The Area is ' + area + ' mm²'
+}
